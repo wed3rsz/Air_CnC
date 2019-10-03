d45ad2c6c479c3581c25638b162a13a9825a6f67
Wederson Medeiros Alves <wedersonflamenguista@gmail.com>
2019-10-03 19:42:52 -0300

    Terceira edição do projeto, inicialização e configuração da aplicação web, com react e nodejs

---
+++ backend/src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path')
+
+const routes = require('./routes');
+
+
+const app = express();
+
+mongoose.connect('mongodb+srv://pinhao4life:pinhao4life@pinhaoforlife-2ek2j.mongodb.net/empreendedorismo?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// GET, POST, PUT, DELETE
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..', 'uploads')))
+app.use(routes);
+
+app.listen(3333);
---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import './styles.css';
+
+export default function Dashboard() {
+    const [spots, setSpots] = useState([]);
+
+    useEffect(() => {
+        async function loadSposts() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/dashboard', {
+                headers: { user_id }
+            });
+
+            setSpots(response.data);
+        }
+        loadSposts();
+    }, []);
+
+    return (
+        <>
+            <ul className="spot-list">
+                {spots.map(spot => (
+                    <li key={spot._id}>
+                        <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price ? `RS${spot.price}/dia` : "GRATUITO" }</span>
+                    </li>
+))}
+            </ul>
+
+            <Link to="/New">
+                <button className="btn">Cadastrar novo spot</button>
+            
+            </Link>
+        </>
+    )
+}
